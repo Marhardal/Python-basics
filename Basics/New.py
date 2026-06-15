c499900479b5e173857430bca5697e1d893f0d55
Martin Harawa <martindharawa@gmail.com>
2026-06-15 15:43:07 +0200

    Initial Commit with a 1 file.

---
+++ Basics/New.py
@@ -0,0 +1,16 @@
+# print("Hello World")
+
+import sys
+
+print("Welcome to Python Programming")
+
+print("This is version " + sys.version)
+
+print("Basic Math Operations")
+
+num1 = 10
+num2 = 21
+
+num = num1 + num2
+
+print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(num))
