3c050c7634a8aa2574edc774b675b6385391e093
Martin Harawa <martindharawa@gmail.com>
2026-06-15 17:20:40 +0200

    Added Types file which has different Types and their convessions.

---
+++ Basics/types.py
@@ -0,0 +1,18 @@
+#int
+print(type(12))
+#float
+print(type(11.9))
+#string
+print(type("Hellow World"))
+#bool
+print(type(True))
+
+print(int(11.9))
+print(str(11.9))
+print(int(True))
+print(bool(0))
+#Error
+#print(int("1 and 2"))
+
+print(type(1/2))
+print(type("hello" == "world"))
