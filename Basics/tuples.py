219fedc9a3211cd932fe76f8c67b4954204a9bc8
Martin Harawa <martindharawa@gmail.com>
2026-06-18 08:56:06 +0200

    Add tuple.py with basic tuple functions

---
+++ Basics/tuples.py
@@ -0,0 +1,17 @@
+tuple1 = (1, 2, 3, 4, 5)
+
+print(type(tuple1))
+
+print(str(tuple1[3]))
+
+print(3 in tuple1)
+
+print(6 in tuple1)
+
+tuple1 = tuple1 + (6,)
+
+print(tuple1)
+
+tuple2 = tuple1 + (7, 8, 9)
+
+print(tuple2)
