57aecb1cc39ea09806401967bd69e6d23b27ba71
Martin Harawa <martindharawa@gmail.com>
2026-06-18 08:45:39 +0200

    add  string ops file.

---
+++ Basics/strOps.py
@@ -0,0 +1,15 @@
+word="Hello World, I am Learning Python."
+
+print(len(word))
+
+print(word[15])
+
+print(word[-15])
+
+print(word * 3)
+
+# print(word.reverse)
+
+word = word + "I am still new to Python."
+
+print(word)
