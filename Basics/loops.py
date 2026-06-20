db1b1e45970fd7fcf6bb36b0389dff3fff819653
Martin Harawa <martindharawa@gmail.com>
2026-06-20 16:35:20 +0200

    Added the loops file with basic loop functions.

---
+++ Basics/loops.py
@@ -0,0 +1,39 @@
+diction = {"Martin": 25, "John": 30, "Alice": 22}
+
+# for i, item in enumerate(diction.items()):
+#     print(item[0], "is", item[1], "years old."+ str(i))
+
+# range(5)
+# i = 0
+# while i <= 10:
+#     print(str(i))
+#     i += 1
+    
+evennumber = 1
+# while evennumber <= 20:
+#     print(evennumber)
+#     evennumber += 2
+
+# while evennumber <=20:
+#     if evennumber / 2 == 0:
+#         print(evennumber)
+#     evennumber += 1
+
+print("Even numbers between 0 and 20 that are not divisible by 3 and less than 10:")
+while evennumber <=20:
+    # if evennumber % 3 != 0:
+    #     continue
+    if evennumber == 10:
+        break
+    # if evennumber / 2 == 0:
+    print(evennumber)
+    evennumber += 1
+
+print("The end")
+
+# primenumber = 0
+
+# while primenumber <= 20:
+#     if primenumber > 1 & primenumber % 2 == 0:
+#         print(primenumber)
+#     primenumber += 1
